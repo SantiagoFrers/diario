43fd87b47ff47bb9f65787f032916893baf827c9
Frers <sfrers@intraudesa.edu.ar>
2021-08-31 11:44:33 -0300

    Se separan las semanas

--- Armado de finales.sql
+++ Armado de finales.sql
@@ -1,17 +1,35 @@
-SELECT distinct ac.n_id_alu_prog, ac.d_registro registro, ap.n_promocion, ap.d_apellidos apellido, ap.d_nombres nombre, (Select p.d_descrip from udesa.programas p where p.c_identificacion = ap.c_identificacion and p.c_programa = ap.c_programa and p.c_orientacion = ap.c_orientacion) carrera, m.d_descred materia, ac.n_id_cur_tipoclase, NVL(decode(ap.n_id_modalidad,922, 'CABA', 923, 'CAMPUS', 924, 'CAMPUS - DH' , 925, 'CABA - DH', 1142, 'CAMPUS - CABA'),'SIN MODALIDAD') modalidad, p.n_id_persona , p.d_apellidos || ',' || p.d_nombres "DOCENTE", ch.c_dia_semanal dia, ch.n_hora_desde "HORA DESDE",
-m.d_descred || '-' || p.n_id_persona || '-' || ch.c_dia_semanal || '-' || ch.n_hora_desde "UNIFICADOR"
+with datos as (SELECT distinct ap.n_id_persona, ac.d_registro registro, ap.n_promocion, ap.d_apellidos apellido, ap.d_nombres nombre, (select nvl(Listagg(UDESA.Devuelve_Desc_Programa(ap2.c_identificacion, ap2.c_programa, ap2.c_orientacion), '/ ') Within Group (Order By 1), '---')prog2 
+                                                                                                                                            from    UDESA.Alumnos_Programas ap2
+                                                                                                                                                        where ap2.n_id_persona = AP.n_id_persona
+                                                                                                                                                        and ap2.c_tipo = 'Alumno'
+                                                                                                                                                        and ap2.n_id_acad_apoyo is null
+                                                                                                                                                        and ap2.f_baja is null
+                                                                                                                                                        and ap2.c_identificacion = 1
+                                                                                                                                                        and ap2.f_graduacion is null ) carrera,
+m.d_descred materia, ac.n_id_cur_tipoclase, NVL(decode(ap.n_id_modalidad,922, 'CABA', 923, 'CAMPUS', 924, 'CAMPUS - DH' , 925, 'CABA - DH', 1142, 'CAMPUS - CABA'),'SIN MODALIDAD') modalidad, p.n_id_persona docente, p.nombre "NOMBRE DOCENTE", 
+decode(ch.c_dia_semanal, 'Lunes', '1 - Lunes', 'Martes', '2 - Martes', 'Miércoles', '3 - Miércoles', 'Jueves', '4 - Jueves', 'Viernes', '5 - Viernes', 'Sin dia' )dia, 
+--ch.n_hora_desde "HORA DESDE", 
+(SELECT min(ch2.n_hora_desde) FROM cursos_horarios ch2 where  ch2.n_id_cur_tipoclase = ch.n_id_cur_tipoclase) "HORA DESDE",
+m.d_descred || '-' || p.n_id_persona || '-' || p.nombre || '-' || ch.c_dia_semanal || '-' || (SELECT min(ch2.n_hora_desde) FROM cursos_horarios ch2 where  ch2.n_id_cur_tipoclase = ch.n_id_cur_tipoclase) "UNIFICADOR", mgd.c_frecuencia
     FROM alumnos_cursos ac,
         alumnos_programas ap,
         materias m,
         cursos c,
         cursos_tipoclase ct,
         cursos_horarios ch,
-        personas p,
-        (SELECT mgd.n_id_cur_tipoclase, mgd.c_tipo_final, max(n_id_persona) n_id_persona
-            FROM  materias_grupos_det mgd
+        (SELECT distinct(devuelve_apenom_per_fic(cp.person_id)) nombre, person_id n_id_persona 
+            FROM sigedu.classification_people cp
+        UNION ALL
+        SELECT D_DOCENTE NOMBRE, n_id_ficticio n_id_persona
+            FROM docentes_ficticios) p,
+        (SELECT mgd2.n_id_cur_tipoclase, mgd2.c_frecuencia, max(mgd2.n_id_persona) n_id_persona
+            FROM  materias_grupos_det mgd2
                 where 1=1
                 and n_id_cal_periodo =:periodo
-                group by mgd.n_id_cur_tipoclase, mgd.c_tipo_final) mgd
+                and ((:final = 'S' and mgd2.c_tipo_final = 'Presencial') or (:final = 'N'))
+                and ((:parcial = 'S' and mgd2.c_tipo_parcial = 'Presencial') or (:parcial = 'N'))
+                --TODO AGREGAR PARCIALES PARA FILTRAR POR FINAL O PARCIAL and mgd2.c_tipo_final = 'Presencial'
+                group by mgd2.n_id_cur_tipoclase, mgd2.c_frecuencia) mgd
         where 1= 1
         and ac.n_id_alu_prog = ap.n_id_alu_prog
         and ac.n_id_materia = m.n_id_materia
@@ -23,23 +41,41 @@ m.d_descred || '-' || p.n_id_persona || '-' || ch.c_dia_semanal || '-' || ch.n_h
         and c.n_id_cal_periodo =:periodo
         and ac.f_baja is null
         and ct.c_tipo_clase = 'Teórica'
-        and mgd.c_tipo_final = 'Presencial'
+        and ap.c_identificacion = 1
         --and ct.n_id_cur_tipoclase = 78711
         --and p.n_id_persona = 121102
         --and ac.n_id_alu_prog in (79917, 99660)
         --and ac.d_registro in ('33081')
-        --AND D_DESCRED = 'D300'
-        order by 1 desc, 2
-       ; --8342
+        --AND D_DESCRED = 'D300'       
+        ),
+        
+parciales_juntos as (select distinct mgd.n_id_cur_tipoclase
+    from materias_grupos_det mgd,
+        (select m.n_id_materia, m.d_descred module_code, d.n_id_persona accademystaff, d.n_sede, d.n_grupo grupo, count(*)
+            from materias_grupos_cab c,
+                materias_grupos_det d,
+                materias m
+                    where c.n_id_cal_periodo= :periodo
+                    and c.n_id_materia=m.n_id_materia
+                    and d.n_id_materia= m.n_id_materia
+                    and c.n_id_mat_grupo_cab=d.n_id_mat_grupo_cab
+                    and d.c_tipo_dictado='PRESENCIAL'
+                    and d.c_tipo_clase = 'TE'
+                         group by m.n_id_materia, m.d_descred, d.n_id_persona, d.n_sede, d.n_grupo
+                         HAVING COUNT(*) > 1
+                         order by 1, 2) x
+            where x.n_id_materia = mgd.n_id_materia
+            and x.accademystaff = mgd.n_id_persona
+            and x.n_sede = mgd.n_sede
+            and x.grupo = mgd.n_grupo
+            and mgd.n_id_cal_periodo= :periodo
+            and mgd.c_tipo_dictado='PRESENCIAL'
+            and mgd.c_tipo_clase = 'TE')
 
-SELECT mgd.n_id_cur_tipoclase, mgd.c_tipo_final, max(n_id_persona) n_id_persona
-    FROM  materias_grupos_det mgd
-        where 1=1
-        and n_id_cal_periodo =:periodo
-        and mgd.n_id_cur_tipoclase = 78711
-        group by mgd.n_id_cur_tipoclase, mgd.c_tipo_final
-        ;
-
-SELECT * 
-    FROM cursos_tipoclase
-        where n_id_cur_tipoclase in (78934, 78916, 78900);
+select d.*, 'Semana 1' as semana, case when c_frecuencia = '1x2' or c_frecuencia = '1x1' then 'Dia fijo' else '-' end "Dia fijo", case when p.n_id_cur_tipoclase is not null then 'Grupos juntos' else '-' end "Grupos juntos"
+    from datos d
+        left join parciales_juntos p on d.n_id_cur_tipoclase = p.n_id_cur_tipoclase
+UNION ALL
+select d.*, 'Semana 2' as semana, case when c_frecuencia = '1x2' or c_frecuencia = '1x1' then 'Dia fijo' else '-' end "Dia fijo", case when p.n_id_cur_tipoclase is not null then 'Grupos juntos' else '-' end "Grupos juntos"
+    from datos d
+        left join parciales_juntos p on d.n_id_cur_tipoclase = p.n_id_cur_tipoclase
